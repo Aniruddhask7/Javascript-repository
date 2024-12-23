483e828694a1a042e28f8db5e626bf26a9adfd51
ANIRUDDHA SANTOSH KANADE <kanadeaniruddha6@gmail.com>
2024-12-23 08:12:43 +0000

    Variables Done

---
+++ javascript/variables.js
@@ -0,0 +1,16 @@
+const accountId  = 12250
+let accountemail = "kanadeaniruddha@gmail.com"
+var accountpassword = "0505052"
+accountCity = "Yavatmal"
+let accountState;
+
+// accountId = 124504
+
+// console.log("accountId")
+
+accountemail = "ask@gmail.com"
+accountpassword = "792111"
+accountCity = "Pune"
+
+console.table([accountId,accountemail,accountpassword,accountCity,accountState]);
+
